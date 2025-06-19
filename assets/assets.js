ea4df00c8fa75293ad478040aeed778e7cd2e7e3
Rahul-singh-mgs <rahulsingh.mgs1998@gmail.com>
2025-06-19 13:15:14 +0530

    First Commit

---
+++ assets/assets.js
@@ -0,0 +1,150 @@
+import user_image from './user-image.png';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu-icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import vscode from './vscode.png';
+import firebase from './firebase.png';
+import figma from './figma.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './logo.png';
+import logo_dark from './logo_dark.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import profile_img from './profile-img.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import profile_icon from './profile_icon.jpg';
+import user_icon_me from './user-icon-me.jpg';
+import logo_r from './logo_r.png';
+import logo_d_r from './logo_d_r.png';
+
+import db from './db.png'
+import draw from './draw.png'
+import docker from './docker.png'
+import eclips from './eclips.png'
+import pgadmin4 from './pgadmin4.png'
+import workbench from './workbench.png'
+import spring from './spring.png'
+
+import f from './f.png'
+import fd from './fd.png'
+import mern from './mern.png'
+
+export const assets = {
+    user_image,
+    code_icon,
+    code_icon_dark,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    vscode,
+    firebase,
+    figma,
+    git,
+    mongodb,
+    right_arrow_white,
+    logo,
+    logo_dark,
+    logo_r,
+    logo_d_r,
+    mail_icon,
+    mail_icon_dark,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark,
+    user_icon_me,
+    profile_icon,
+    db,
+    draw,
+    docker,
+    eclips,
+    pgadmin4,
+    workbench,
+    spring,
+    f,fd,mern
+};
+
+export const workData = [
+    {
+        title: 'Grocery app',
+        description: 'Web Design',
+        bgImage: '/g2.png',
+        link: 'https://minutemart.vercel.app/'
+    },
+    {
+        title: 'Portfolio',
+        description: 'Web App',
+        bgImage: 'portfolio.png',
+        link: 'https://portfolio-rahul-singh-five.vercel.app/'
+    },
+    {
+        title: 'Hotel Booking site',
+        description: 'Web Design | Coming Soon',
+        bgImage: '/hotel.png',
+        link: ''
+    },
+    {
+        title: 'Sounds and tools site',
+        description: 'Web Design',
+        bgImage: '/sp2.png',
+        link: 'https://frontend-avian-tech.vercel.app/'
+    },
+]
+
+export const serviceData = [
+    { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: '' },
+    { icon: assets.fd, title: 'Full stack', description: 'The ability to work on both the frontend and backend  of a web application...', link: '' },
+    { icon: assets.f, title: 'Frontend design', description: 'Frontend development involves creating the visual and interactive parts of a website...', link: '' },
+    { icon: assets.mern, title: 'MERN stack', description: 'The MERN Stack is a popular web development stack consisting of...', link: '' },
+]
+
+export const infoList = [
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js, Angular, Java, Basic Python' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+];
+
+export const toolsData = [
+    assets.vscode, assets.mongodb, assets.figma, assets.git, assets.draw,
+    assets.docker, assets.eclips, assets.pgadmin4, assets.workbench, assets.spring
+];
